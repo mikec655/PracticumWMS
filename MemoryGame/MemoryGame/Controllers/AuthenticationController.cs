1bcf2a3b731a06770335a464addaa8721be1a3a9
NiekS <32094562+pelikaanjpeg@users.noreply.github.com>
2019-06-02 21:54:12 +0200

    Added start of JWT and DbContext

---
+++ MemoryGame/MemoryGame/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MemoryGame.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemoryGame.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthenticationController : Controller
+    {
+        [HttpGet("[action]")]
+        public User Authenticate(dynamic user)
+        {
+            var newUser = new User();
+            return newUser;
+        }
+    }
+}
