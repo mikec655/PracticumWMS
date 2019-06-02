94dd85f1ecbf22cbef18bc0875c44dc91885d8fb
Mike Clarke <mikec655@gmail.com>
2019-06-02 21:48:03 +0200

    Start User classes

---
+++ MemoryGame/MemoryGame/Models/User.cs
@@ -0,0 +1,16 @@
+
+namespace MemoryGame.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
